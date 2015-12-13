049bc1df2bded77940369eb6015a29156c26cbc1
hec17x <hec.medina.trabajo@gmail.com>
2015-12-13 02:05:39 +0100

    Formulario de inicio. Versión 0.2
    Añadida animación.
    Falta:
    -Selector de color

---
+++ iManagerGen/MVCApp/Scripts/instalacion.js
@@ -0,0 +1,42 @@
+﻿function iniciar() {
+
+    document.getElementById('apariencia').style.display = "none";
+}
+
+
+function cambiar()
+{    
+  
+
+
+    if (document.getElementById('apariencia').style.display == "none")
+    {
+        document.getElementById('datos').className = "register-box-body animated fadeOutRightBig";
+        document.getElementById('apariencia').className = "register-box-body animated fadeInLeft";
+        
+        setTimeout(function () {
+            document.getElementById('apariencia').style.display = "block";
+            document.getElementById('datos').style.display = "none";
+        },400);
+            
+        
+        
+    }
+        else
+        {
+            document.getElementById('datos').className = "register-box-body animated fadeInRight";
+            document.getElementById('apariencia').className = "register-box-body animated fadeOutLeftBig";
+            setTimeout(function () {
+                document.getElementById('apariencia').style.display = "none";
+                document.getElementById('datos').style.display = "block";
+            }, 400);
+
+         
+        }
+
+
+
+    
+
+    
+}
