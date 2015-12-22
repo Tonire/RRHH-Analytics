7ebca0a1f76a313e8116ff8fa6d841dcb9a24800
Juliyo <julio17795@hotmail.com>
2015-12-22 19:24:10 +0100

    modelo mensajes

---
+++ iManagerGen/MVCApp/Models/VerMensajesModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCApp.Models {
+    public class VerMensajesModels {
+        
+        public long Cuenta { get; set; }
+        public string IdMensaje { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Asunto { get; set; }
+        public string Contenido { get; set; }
+        public string Tiempo { get; set; }
+
+    }
+}
