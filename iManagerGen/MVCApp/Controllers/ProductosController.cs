fffec18654a865dce1ae9f213a379123dc6a2ac3
Toni <tonire.rebollo@gmail.com>
2015-12-27 02:54:41 +0100

    Ver productos terminado

---
+++ iManagerGen/MVCApp/Controllers/ProductosController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IManagerGenNHibernate.CEN.IManager;
+using IManagerGenNHibernate.EN.IManager;
+namespace MVCApp.Controllers
+{
+    public class ProductosController : Controller
+    {
+        //
+        // GET: /Productos/
+
+        public ActionResult Index()
+        {
+            ProductoCEN productoCEN = new ProductoCEN();
+            IEnumerable<ProductoEN> listaProductos = productoCEN.GetAllProductos(0,-1).ToList();
+            return View(listaProductos);
+        }
+
+        //
+        // GET: /Productos/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Productos/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Productos/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Productos/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Productos/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Productos/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Productos/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ iManagerGen/MVCApp/Models/ProductoModels.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCApp.Models {
+    public class ProductoModels {
+        [Display(Prompt = "Referencia", Description = "Referencia del producto", Name = "Referencia")]
+        [Required(ErrorMessage = "Debe asignar una referencia al producto")]
+        [Range(minimum: 0, maximum: Int32.MaxValue, ErrorMessage = "Debes introducir una referencia válida.")]
+        public int Referencia { get; set; }
+
+        [Display(Prompt = "Nombre", Description = "Nombre del producto", Name = "Nombre")]
+        [Required(ErrorMessage = "Debe indicar un nombre para el producto")]
+        [StringLength(maximumLength: 200, ErrorMessage = "El nombre no puede tener más de 200 caracteres")]
+        public string Nombre { get; set; }
+
+        [Display(Prompt = "Marca", Description = "Marca del producto", Name = "Marca")]
+        [Required(ErrorMessage = "Debe indicar una marca para el producto")]
+        [StringLength(maximumLength: 200, ErrorMessage = "La marca no puede tener más de 200 caracteres")]
+        public string Marca { get; set; }
+
+        [Display(Prompt = "Precio de compra", Description = "Precio de compra del producto", Name = "PrecioCompra")]
+        [Required(ErrorMessage = "Debe indicar un nombre para el artículo")]
+        public int PrecioCompra { get; set; }
+
+        [Display(Prompt = "Precio de venta", Description = "Precio de venta del producto", Name = "PrecioVenta")]
+        [Required(ErrorMessage = "Debe indicar un precio de venta para el producto")]
+        public int PrecioVenta { get; set; }
+
+        [Display(Prompt = "Stock", Description = "Stock del producto", Name = "Stock")]
+        [Required(ErrorMessage = "Debe indicar un stock para el producto")]
+        public int Stock { get; set; }
+
+    }
+}
